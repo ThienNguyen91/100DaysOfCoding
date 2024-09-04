14e447f88851d2e570a4240967c4ec2ac2045dba
Thien Nguyen <giathiennc006@gmail.com>
2024-09-04 23:18:19 +0700

    Day 17

--- Day 17/main.py
+++ Day 17/main.py
@@ -2,12 +2,8 @@ from data import  question_data
 from question_model import AddQuestion
 from quiz_brain import QuizBrain
 
-question_bank = []
-for eachQuestion in  question_data:
-    question_text = eachQuestion["text"]
-    question_answer = eachQuestion["answer"]
-    newQuestion = AddQuestion(question_text, question_answer)
-    question_bank.append(newQuestion)
+
+question_bank = [AddQuestion(q["text"], q["answer"]) for q in question_data]
 Start = QuizBrain(question_bank)
 while Start.next_question():
     pass

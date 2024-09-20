b189fda3faaeca292916ecdf157188fad96b7da0
Thien Nguyen <giathiennc006@gmail.com>
2024-09-20 23:07:08 +0700

    Day 24

--- Day 24/Mail Merge Project Start/main.py
+++ Day 24/Mail Merge Project Start/main.py
@@ -1,14 +1,29 @@
 #TODO: Create a letter using starting_letter.txt
+#constant
+import os.path
+
 PLACEHOLDER = "[name]"
-sample = open("./Input/Letters/starting_letter.txt")
-data = sample.read()
-with open("./Input/Names/invited_names.txt", "r") as name_list:
+INPUT_NAMES_FILE = "./Input/Names/invited_names.txt"
+INPUT_LETTER_FILE = "./Input/Letters/starting_letter.txt"
+OUTPUT_DIR = "./Output/ReadyToSend/"
+
+
+if not os.path.exists(OUTPUT_DIR):
+    os.makedirs(OUTPUT_DIR)
+
+#Read data from sample letter
+with open(INPUT_LETTER_FILE) as sample:
+    data = sample.read()
+
+#read all names
+with open(INPUT_NAMES_FILE) as name_list:
     names = name_list.readlines()
 
-
+#Create personalized letters
 for name in names:
-    stripped_name = name.strip()
-    with open(f"./Output/ReadyToSend/{stripped_name}", "w") as new_letter:
-        personalized_letter = data.replace(PLACEHOLDER, stripped_name)
+    name = name.strip()
+    personalized_letter = data.replace(PLACEHOLDER, name)
+
+    output_file = os.path.join(OUTPUT_DIR, f"{name}.txt")
+    with open(output_file, "w") as new_letter:
         new_letter.write(personalized_letter)
-sample.close()

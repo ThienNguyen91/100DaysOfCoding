b19bed3a75092b747e869ae76fc2d27b21b35edc
Thien Nguyen <giathiennc006@gmail.com>
2024-10-01 19:30:19 +0700

    Day 31

--- Day 31/main.py
+++ Day 31/main.py
@@ -1,12 +1,12 @@
 from tkinter import *
-from random import randint
+from random import choice
 import pandas
-from pandas import *
 DATA_DIR = "data/japan_word.csv"
 
 with open(DATA_DIR, "r") as file:
     data = pandas.read_csv(file)
     data_dict = data.to_dict(orient= "records")
+current_card = {}
 BACKGROUND_COLOR = "#B1DDC6"
 window = Tk()
 window.title("Flashy")
@@ -14,14 +14,26 @@ window.config(padx= 50, pady= 50, bg=BACKGROUND_COLOR)
 
 #
 def change_word():
-    random_num = randint(0, len(data_dict)-1)
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = choice(data_dict)
+    canvas.itemconfig(img, image=front_bg)
+    canvas.itemconfig(language_text, text="French", fill="black")
+    canvas.itemconfig(vocab_text, text=current_card["French"], fill="black")
+    flip_timer = window.after(3000, func= flip_card)
+def flip_card():
+    canvas.itemconfig(language_text, text= "English", fill= "white")
+    canvas.itemconfig(vocab_text, text= current_card["English"], fill= "white")
+    canvas.itemconfig(img, image=back_bg)
 
+flip_timer = window.after(3000, func= flip_card)
 
 
 canvas = Canvas(width=800,height=526)
-green_bg = PhotoImage(file="images/card_front.png")
-canvas.create_image(400,263, image=green_bg)
-japan_text = canvas.create_text(400, 150, text="Japan", font= ("ariel", 40, "italic"))
+front_bg = PhotoImage(file="images/card_front.png")
+back_bg = PhotoImage(file="images/card_back.png")
+img = canvas.create_image(400,263, image=front_bg)
+language_text = canvas.create_text(400, 150, text="French", font= ("ariel", 40, "italic"))
 vocab_text = canvas.create_text(400, 263, text= "", font= ("ariel", 60, "bold"))
 canvas.config(bg= BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(column =0, row =1, columnspan = 2)
@@ -37,10 +49,7 @@ right_img = PhotoImage(file="images/right.png")
 right_button = Button(image= right_img, command=change_word)
 right_button.config(bg= BACKGROUND_COLOR, highlightthickness=0)
 right_button.grid(column= 1, row= 2)
-
-
-
-
+change_word()
 
 
 
